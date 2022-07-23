f1177046b6c3864a51bea6db419598361ddf995a
KreskoLab <redeezol@gmail.com>
2022-07-23 17:23:52 +0300

    feat (app): connect components

--- app.vue
+++ app.vue
@@ -1,195 +1,71 @@
-<template>
-  <main class="min-h-screen">
-    <section
-      class="bg-gradient-to-tr from-stone-900 to-orange-900 bg-noise h-screen"
-    >
-      <div
-        class="flex flex-col justify-center space-y-2 h-full max-w-screen-2xl container mx-auto px-4"
-      >
-        <h1 class="text-5xl lg:text-9xl text-center text-amber-50 bg-image">
-          Andrii Orlov
-        </h1>
-
-        <h2
-          class="text-amber-100 lg:text-lg text-right text-opacity-anim mt-1 mx-4"
-        >
-          Yes... Another one self taught developer
-        </h2>
-      </div>
-    </section>
-
-    <About />
-
-    <TheColors />
-
-    <section
-      id="projects"
-      class="bg-gradient-to-tr from-stone-800 to-stone-900 bg-noise min-h-screen"
-    >
-      <div
-        class="flex flex-col items-center h-full max-w-screen-2xl container mx-auto"
-      >
-        <h3 class="text-5xl lg:text-9xl text-amber-50 bg-image mt-16">
-          Projects
-        </h3>
-
-        <div
-          class="flex flex-col lg:flex-row lg:flex-wrap justify-center gap-x-4 gap-y-16 py-12"
-        >
-          <div
-            class="w-full lg:w-[900px]"
-            v-for="card in cards"
-            :key="card.title"
-          >
-            <Card
-              :title="card.title"
-              :subtitle="card.subtitle"
-              :images="card.images"
-              :frontend="card.frontend"
-              :backend="card.backend"
-              :link="card.link"
-              :github="card.github"
-            />
-          </div>
-        </div>
-      </div>
-    </section>
-  </main>
-</template>
-
-<script setup lang="ts">
-const cards = [
-  {
-    title: "Priceefy",
-    subtitle: "App for tracking food prices in Ukraine",
-    frontend: "Frontend: Nuxt 3, Tailwind CSS, Pinia, ApexCharts, SSR",
-    backend: "Backend: Nest, MongoDB, Jest, GitHub Actions",
-    github: "https://github.com/KreskoLab/Priceefy",
-    link: "https://priceefy.com",
-    images: [
-      "https://res.cloudinary.com/kresko/image/upload/v1652807950/Priceefy-thumb_oojyax.png",
-      "https://res.cloudinary.com/kresko/image/upload/v1653602172/Pepsi_max_-_Priceefy_jls5gl.png",
-      "https://res.cloudinary.com/kresko/image/upload/v1653603005/Pepsi_-_Priceefy_cpg6pq.png",
-    ],
-  },
-
-  {
-    title: "Balloo",
-    subtitle: "Mock of balloons e-commerce website",
-    frontend: "Frontend: Nuxt 2, Windi CSS, Vuex, SSR",
-    backend: "Backend: Fastify, Passport, MongoDB",
-    github: "https://github.com/KreskoLab/balloo",
-    link: "https://balloo.andrii.site",
-    images: [
-      "https://res.cloudinary.com/kresko/image/upload/v1653059804/balloo_uarh1b.png",
-      "https://res.cloudinary.com/kresko/image/upload/v1653600865/Balloo_1_uslptr.png",
-    ],
-  },
-
-  {
-    title: "Dumka",
-    subtitle: "UGC engine",
-    frontend: "Frontend: Nuxt 3, Tailwind CSS, Pinia",
-    backend: "Backend: Nuxt nitro, MongoDB",
-    github: "",
-    link: "",
-    images: [
-      "https://res.cloudinary.com/kresko/image/upload/v1652807950/Priceefy-thumb_oojyax.png",
-    ],
-  },
-
-  {
-    title: "Kemcio",
-    subtitle: "App for manage IOT devices",
-    frontend: "Frontend: Vite, Windi CSS, Pinia, SPA",
-    backend: "Backend: Nest, MongoDB, Jest, Microservices",
-    images: [
-      "https://res.cloudinary.com/kresko/image/upload/v1652807950/Priceefy-thumb_oojyax.png",
-    ],
-  },
-];
-
-onMounted(() => {
-  const section = document.getElementById("projects");
-
-  const observer = new IntersectionObserver(
-    (entries) => {
-      const firstEntry = entries[0];
-
-      if (firstEntry.isIntersecting) {
-        firstEntry.target.children[0].children[0].classList.add(
-          "text-opacity-anim"
-        );
-      } else {
-        firstEntry.target.children[0].children[0].classList.remove(
-          "text-opacity-anim"
-        );
-      }
-    },
+<script setup>
+useHead({
+  title: "Andrii Orlov - Developer",
+  link: [{ rel: "icon", type: "image/svg", href: "/favicon.svg" }],
+  meta: [
     {
-      root: null,
-      threshold: 0.1,
-    }
-  );
-
-  observer.observe(section);
+      name: "description",
+      content: "Full stack developer, JavaScript lover",
+    },
+  ],
 });
 </script>
 
+<template>
+  <main class="min-h-screen scroll-smooth bg-black overflow-y-auto">
+    <div
+      class="container mx-auto max-w-screen-md min-w-[320px] py-16 px-4 sm:px-0 h-full"
+    >
+      <section class="z-50">
+        <div class="flex items-center space-x-6">
+          <img
+            width="64"
+            height="64"
+            src="https://avatars.githubusercontent.com/u/29521254?v=4"
+            alt="kresko`s avatar"
+            class="rounded-xl ring-[5px] ring-sky-500/60 z-50"
+          />
+
+          <div class="text-sky-50/80 font-medium">
+            <h1 class="text-3xl tracking-wider font-bold">Andrii Orlov</h1>
+            <p>Full stack developer</p>
+          </div>
+        </div>
+
+        <div class="text-sky-100/90 my-10 text-lg tracking-wide">
+          <p>Hello. I`m Andrii from Kyiv, Ukraine.</p>
+          <p>I`m interesed in building web apps and some little in IoT.</p>
+        </div>
+
+        <TheLinks />
+      </section>
+
+      <section class="z-50">
+        <TheAbout />
+      </section>
+
+      <section class="z-50">
+        <TheProjects />
+      </section>
+
+      <section class="fixed top-0 left-0 w-full h-full">
+        <div
+          class="gradient w-full bottom-0 left-0 blur-[300px] sm:w-96 sm:blur-[500px] !bg-pink-600"
+        />
+        <div
+          class="gradient w-full top-0 right-0 blur-[300px] sm:blur-[1000px] sm:w-96 !bg-teal-600"
+        />
+      </section>
+    </div>
+  </main>
+</template>
+
 <style scoped>
-.bg-image {
-  background: url(~/assets/noise.png),
-    linear-gradient(
-      103deg,
-      #b7ab99 3.56%,
-      #ffa235 32.81%,
-      #cf5c2a 56.49%,
-      #f6543c 92.71%
-    );
-  -webkit-background-clip: text;
-  background-clip: text;
-  color: transparent;
-}
-
-.bg-noise {
-  background-image: url(~/assets/noise.png) center center repeat;
-  background-size: 0.2%;
-}
-
-h1 {
-  animation: rotateText 0.5s ease;
-}
-
-html {
-  scroll-behavior: smooth;
-}
-
-.text-opacity-anim {
-  animation: opacityText 1s ease;
-}
-
-@keyframes rotateText {
-  0% {
-    transform-origin: bottom;
-    opacity: 0;
-    transform: rotate(-5deg) scale(0.9) translate(10px, 50px);
-  }
-
-  100% {
-    transform-origin: center;
-    transform: rotate(0) scale(1) translate(0);
-  }
-}
-
-@keyframes opacityText {
-  0% {
-    opacity: 0;
-    transform: translateX(10px);
-  }
-
-  100% {
-    opacity: 1;
-    transform: translateX(0);
-  }
+.gradient {
+  position: absolute;
+  z-index: 0;
+  opacity: 0.8;
+  padding-bottom: 100%;
+  transform: scale(0.8);
 }
 </style>
