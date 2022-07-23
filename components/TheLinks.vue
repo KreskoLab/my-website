e75855cff4c462c90c7f148326114db7b2bb2518
KreskoLab <redeezol@gmail.com>
2022-07-23 17:07:17 +0300

    feat: add TheLinks component

---
+++ components/TheLinks.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="flex items-center space-x-8">
+    <a target="_blank" class="group" href="mailto:andrii.orlov@proton.me">
+      <svg
+        class="!p-0.5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+        />
+      </svg>
+      <span>Email</span>
+    </a>
+
+    <a target="_blank" class="group" href="https://github.com/KreskoLab">
+      <svg class="!p-1" width="32" height="32" viewBox="0 0 16 16">
+        <path
+          fill="#f8fafc"
+          d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59c.4.07.55-.17.55-.38c0-.19-.01-.82-.01-1.49c-2.01.37-2.53-.49-2.69-.94c-.09-.23-.48-.94-.82-1.13c-.28-.15-.68-.52-.01-.53c.63-.01 1.08.58 1.23.82c.72 1.21 1.87.87 2.33.66c.07-.52.28-.87.51-1.07c-1.78-.2-3.64-.89-3.64-3.95c0-.87.31-1.59.82-2.15c-.08-.2-.36-1.02.08-2.12c0 0 .67-.21 2.2.82c.64-.18 1.32-.27 2-.27c.68 0 1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82c.44 1.1.16 1.92.08 2.12c.51.56.82 1.27.82 2.15c0 3.07-1.87 3.75-3.65 3.95c.29.25.54.73.54 1.48c0 1.07-.01 1.93-.01 2.2c0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+        />
+      </svg>
+      <span>GitHub</span>
+    </a>
+
+    <a target="_blank" class="group" href="/Andrii_Orlov_CV.pdf">
+      <svg
+        class="!p-0.5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+        />
+      </svg>
+      <span>CV</span>
+    </a>
+  </div>
+</template>
+
+<style scoped>
+svg {
+  @apply h-7 w-7 text-sky-50/90 rounded-lg bg-gradient-to-tr from-fuchsia-600 to-rose-600 transform duration-300 group-hover:scale-110;
+}
+
+a {
+  @apply flex items-center space-x-2 text-lg text-sky-100/80 cursor-pointer z-50;
+}
+
+span {
+  @apply transition duration-150 group-hover:text-sky-200;
+}
+</style>
